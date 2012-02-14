15561050cd14df006ee5cf876ef1bedbbc0150f1
beepscore <support@beepscore.com>
2012-02-13 23:11:04 -0800

    Exercise 7.

---
+++ ex7.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+
+# puts 10 periods
+puts "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using print instead of puts here.
+# change it to puts and see what happens.
+# print inhibits line feed, output on one line "CheeseBurger"
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12 
+
+puts
+# output on two lines
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12 
+
+# this is just polite use of the terminal, try removing it
+# if comment out puts, don't get blank line at end of output
+puts
