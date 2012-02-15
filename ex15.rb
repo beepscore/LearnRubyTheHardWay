7d27e64504320bce7cd3e658879d00cde0e11bd8
beepscore <support@beepscore.com>
2012-02-14 22:28:44 -0800

    Exercise 15.

---
+++ ex15.rb
@@ -0,0 +1,18 @@
+#!usr/bin/env ruby
+
+filename = ARGV.first
+
+prompt = "> "
+txt = File.open(filename)
+
+puts "Here's your file: #{filename}"
+puts txt.read()
+
+puts "I'll also ask you to type it again:"
+print prompt
+#ruby expects file name without delimiters
+file_again = STDIN.gets.chomp()
+
+txt_again = File.open(file_again)
+
+puts txt_again.read()
