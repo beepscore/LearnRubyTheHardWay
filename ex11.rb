7fc2b481013323d8e55487a20c36122eadfa83cc
beepscore <support@beepscore.com>
2012-02-14 20:54:56 -0800

    Exercise 11

--- ex11.rb
+++ ex11.rb
@@ -2,7 +2,11 @@
 
 # References
 
+# use print instead of puts to avoid newline
 print "How old are you? "
+# http://ruby-doc.org/core-1.9.3/IO.html#method-i-gets
+# http://ruby-doc.org/core-1.9.3/String.html#method-i-chomp
+# get the string, chomp removes carriage return characters.
 age = gets.chomp()
 print "How tall are you? "
 height = gets.chomp()
@@ -10,4 +14,3 @@ print "How much do you weigh? "
 weight = gets.chomp()
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
-
