28f3ffa8ed83e1e3590fe629bd4198d2aab9af48
beepscore <support@beepscore.com>
2012-02-14 22:02:08 -0800

    Extra Credit: Combine using command line arguments with using STDIN.gets.chomp()

--- ex13.rb
+++ ex13.rb
@@ -7,3 +7,10 @@ puts "The script is called #{$0}"
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+
+print "Please enter a fourth value: "
+# unlike previous exercise, must specify STDIN
+# without STDIN, I think ruby tries to use parameter "first" as a file name to read from a file.
+fourthVariable = STDIN.gets.chomp()
+
+puts "Your fourth variable is #{fourthVariable}"
