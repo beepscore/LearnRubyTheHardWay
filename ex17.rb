1a7f75d3e3f841bb806c2115e47a4009ccf750ae
beepscore <support@beepscore.com>
2012-02-16 23:29:06 -0800

    Exercise 17 extra credit combine two lines into one.

--- ex17.rb
+++ ex17.rb
@@ -6,8 +6,10 @@ from_file, to_file = ARGV
 puts "Copying from #{from_file} to #{to_file}"
 
 # we could do these two on one line too, how?
-input = File.open(from_file)
-indata = input.read()
+#input = File.open(from_file)
+#indata = input.read()
+# this collapses two lines into one, but makes closing awkward
+indata = File.open(from_file).read()
 
 puts "The input file is #{indata.length} bytes long"
 
@@ -22,4 +24,8 @@ output.write(indata)
 puts "Alright, all done."
 
 output.close()
-input.close()
+
+#input.close()
+# I think this closes the open file, 
+# doesn't leave it open by opening twice and closing once.
+File.open(from_file).close()
