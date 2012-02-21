a2484ba08100d7d856d2feed52f7303f66f0657c
beepscore <support@beepscore.com>
2012-02-20 21:54:24 -0800

    Extra credit: Use +=

--- ex20.rb
+++ ex20.rb
@@ -37,10 +37,10 @@ puts "Let's print three lines:"
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
 current_file.close()
