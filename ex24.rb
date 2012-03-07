12155aced21b2a36714f34a3420a1583b5941a36
beepscore <support@beepscore.com>
2012-03-06 21:55:20 -0800

    Fix typo change "ther" to "there"

--- ex24.rb
+++ ex24.rb
@@ -10,7 +10,7 @@ with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
-\n\t\twhere ther is none.
+\n\t\twhere there is none.
 
 MULTI_LINE_STRING
 
