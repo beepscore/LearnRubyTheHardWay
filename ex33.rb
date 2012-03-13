74dc4aa05454cb8c4d0e4ee1e0798bdb7dba232b
beepscore <support@beepscore.com>
2012-03-12 22:29:14 -0700

    Extra credit- implement a function

--- ex33.rb
+++ ex33.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 
-i = 0
-numbers = []
+def configure_array(number_of_elements)
+  i = 0
+  numbers = []
+  while i < number_of_elements
+    puts "At the top i is #{i}"
+    numbers.push(i)
+    i = i + 1
+    puts "Numbers now: #{numbers}"
+    puts "At the bottom i is #{i}"
+  end
 
-while i < 6
-  puts "At the top i is #{i}"
-  numbers.push(i)
-
-  i = i + 1
-  puts "Numbers now: #{numbers}"
-  puts "At the bottom i is #{i}"
+  puts "The numbers: "
+  for num in numbers
+    puts num
+  end
 end
 
-puts "The numbers: "
-
-for num in numbers
-  puts num
-end
+configure_array(6)
+configure_array(3)
+configure_array(11)
