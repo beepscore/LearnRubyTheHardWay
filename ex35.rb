264e18e7bc732ed98db3c5a104414e62c3d45c4d
beepscore <support@beepscore.com>
2012-03-13 21:02:19 -0700

    Extra credit. Improve prompts. Accept more answers.

--- ex35.rb
+++ ex35.rb
@@ -5,10 +5,10 @@ def prompt()
 end
 
 def gold_room()
-  puts "This room is full of gold. How much do you take?"
+  puts "This room is full of gold. How many pieces do you take?"
 
   prompt; next_move = gets.chomp
-  if next_move.include? "0" or next_move.include? "1"
+  if next_move == 0 or next_move.to_i() != 0 
     how_much = next_move.to_i()
   else
     dead("Man, learn to type a number.")
@@ -27,19 +27,29 @@ def bear_room()
   puts "The bear has a bunch of honey."
   puts "The fat bear is in front of another door."
   puts "How are you going to move the bear?"
+  puts "1 take honey"
+  puts "2 taunt bear"
+  puts "3 open door"
+
   bear_moved = false
 
   while true
     prompt; next_move = gets.chomp
 
-    if next_move == "take_honey"
+    if next_move == "1" or next_move == "take honey"
       dead("The bear looks at you then slaps your face off.")
-    elsif next_move == "taunt bear" and not bear_moved
+    elsif ((next_move == 2 or
+            next_move.downcase == "taunt bear") and 
+            not bear_moved)
       puts "The bear has moved from the door. You can go through it now."
       bear_moved = true
-    elsif next_move == "taunt bear" and bear_moved
+    elsif ((next_move == 2 or
+            next_move == "taunt bear") and 
+            bear_moved)
       dead("The bear giets pissed off and chews your leg off.")
-    elsif next_move == "open door" and bear_moved
+    elsif ((next_move == 3 or
+            next_move == "open door") and 
+            bear_moved)
       gold_room()
     else
       puts "I got no idea what that means."
@@ -54,9 +64,9 @@ def cthulhu_room()
 
   prompt; next_move = gets.chomp
 
-  if next_move.include? "flee"
+  if next_move.downcase.include? "flee"
     start()
-  elsif next_move.include? "head"
+  elsif next_move.downcase.include? "head"
     dead("Well that was tasty!")
   else
     cthulu_room()
@@ -75,9 +85,9 @@ def start()
 
   prompt; next_move = gets.chomp
 
-  if next_move == "left"
+  if next_move.downcase.include? "left"
     bear_room()
-  elsif next_move == "right"
+  elsif next_move.downcase.include? "right"
     cthulhu_room()
   else
     dead("You stumble around until you starve.")
