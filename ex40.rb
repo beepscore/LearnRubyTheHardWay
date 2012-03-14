8f09850669c55295c41b39b2156fad8580e8eafb
beepscore <support@beepscore.com>
2012-03-14 00:17:27 -0700

    Add comments and statement explaining how program works.

--- ex40.rb
+++ ex40.rb
@@ -16,14 +16,22 @@ def find_city(map, state)
 end
 
 # ok pay attention!
+# In cities hash, add entry with key :find and value method(:find_city)
 cities[:find] = method(:find_city)
+# log cities to show result
+puts cities
+# {"CA"=>"San Francisco", "MI"=>"Detroit", "FL"=>"Jacksonville", "NY"=>"New York", "OR"=>"Portland", :find=>#<Method: Object#find_city>}
 
 while true
   print "State? (ENTER to quit) "
   state = gets.chomp
 
+  # if user types return, state will be empty and execution will break from while loop
   break if state.empty?
 
-  # this line is the most importan ever! study!
+  # this line is the most important ever! study!
+  # this evaluates to puts find_city(cities, state)
   puts cities[:find].call(cities, state)
+  # show equivalence
+  puts find_city(cities, state)
 end
