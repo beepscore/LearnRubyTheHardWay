4adeac1faedb9b21a8817adf11f77830075913c6
beepscore <support@beepscore.com>
2012-03-16 21:24:57 -0700

    Change more multi-line puts to use heredocs.

--- ex41.rb
+++ ex41.rb
@@ -36,29 +36,35 @@ def central_corridor()
   action = gets.chomp()
 
   if action == "shoot!"
-    puts "Quick on the draw you yank out your blaster and fire it at the Gothon."
-    puts "His clown costom is flowing and moving around his body, which throws"
-    puts "off your aim. Your laser hits his costume but misses him entirely. This"
-    puts "makes him fly into an insane rage and blast you repeatedly in the face until"
-    puts "you are dead. Then he eats you."
+    puts <<-SHOOT_HEREDOC
+      Quick on the draw you yank out your blaster and fire it at the Gothon.
+      His clown costom is flowing and moving around his body, which throws
+      off your aim. Your laser hits his costume but misses him entirely. This
+      makes him fly into an insane rage and blast you repeatedly in the face until
+      you are dead. Then he eats you.
+    SHOOT_HEREDOC
     return :death
 
   elsif action == "dodge!"
-    puts "Like a world class boxer you dodge, weave, slip and slide right"
-    puts "as the Gothon's blaster cranks a laser past your head."
-    puts "In the middle of your artful dodge your foot slips and you"
-    puts "bang your head on hte metal wall and pass out."
-    puts "You wake up shortly after only to die as the Gothon stomps on"
-    puts "your head and eats you."
+    puts <<-DODGE_HEREDOC
+      Like a world class boxer you dodge, weave, slip and slide right
+      as the Gothon's blaster cranks a laser past your head.
+      In the middle of your artful dodge your foot slips and you
+      bang your head on hte metal wall and pass out.
+      You wake up shortly after only to die as the Gothon stomps on
+      your head and eats you.
+    DODGE_HEREDOC
     return :death
 
   elsif action == "tell a joke"
-    puts "Lucky for you they made you learn Gothon insults in the academy."
-    puts "You tell the one Gothon joke you know:"
-    puts "Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr."
-    puts "The Gothon stops, tries not to laugh, then busts out laughing and can't move."
-    puts "While he's laughing you run up and shoot him square in the head"
-    puts "putting him down, then jump through the Weapon Armory door."
+    puts <<-JOKE_HEREDOC
+      Lucky for you they made you learn Gothon insults in the academy.
+      You tell the one Gothon joke you know:
+      Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.
+      The Gothon stops, tries not to laugh, then busts out laughing and can't move.
+      While he's laughing you run up and shoot him square in the head
+      putting him down, then jump through the Weapon Armory door.
+    JOKE_HEREDOC
     return :laser_weapon_armory
 
   else
@@ -68,13 +74,14 @@ def central_corridor()
 end
 
 def laser_weapon_armory()
-  puts "You do a dive roll into the Weapon Armory, crouch and scan the room"
-  puts "for more Gothons that might be hiding. It's dead quiet, too quiet."
-  puts "You stand up and run to the far side of the room and find the"
-  puts "neutron bomb in its container. There's a keypad lock on the box"
-  puts "and you need the code to get the bomb out. If you get the code"
-  puts "wrong 10 times then the lock closes forever and you can't get the bomb. The code is 3 digits."
-
+  puts <<-LASER_HEREDOC
+    You do a dive roll into the Weapon Armory, crouch and scan the room
+    for more Gothons that might be hiding. It's dead quiet, too quiet.
+    You stand up and run to the far side of the room and find the
+    neutron bomb in its container. There's a keypad lock on the box
+    and you need the code to get the bomb out. If you get the code
+    wrong 10 times then the lock closes forever and you can't get the bomb. The code is 3 digits.
+  LASER_HEREDOC
   # code = "%s%s%s" % [rand(9)+1, rand(9)+1, rand(9)+1]
   # Make the game easier than instructions say. 
   # Give the player 10 guesses
@@ -106,8 +113,8 @@ def laser_weapon_armory()
   winning_combinations = sorted_combinations_keys[0...number_of_winning_combinations]
   # now re-sort winning_combinations in numerical order
   winning_combinations.sort!
-  puts "winning_combinations"
-  pp winning_combinations
+  #puts "winning_combinations"
+  #pp winning_combinations
 
   for guesses in 1..10 do 
     print "[keypad]> "
@@ -120,9 +127,11 @@ def laser_weapon_armory()
   end
 
   if winning_combinations.include? guess    
-    puts "The container clicks open and the seal breaks, letting gas out."
-    puts "You grab the neutron bomb and run as fast as you can to the"
-    puts "bridge where you must place it in the right spot."
+    puts <<-BOMB_UNLOCK_HEREDOC
+     The container clicks open and the seal breaks, letting gas out.
+     You grab the neutron bomb and run as fast as you can to the
+     bridge where you must place it in the right spot.
+    BOMB_UNLOCK_HEREDOC
     return :the_bridge
   else
     puts "The lock buzzes one last time and then you hear a sickening"
@@ -134,34 +143,40 @@ def laser_weapon_armory()
 end
 
 def the_bridge()
-  puts "You burst onto the Bridge with the neutron destruct bomb"
-  puts "under your arm and surprise 5 Gothons who are trying to"
-  puts "take control of the ship. Each of them has an even uglier"
-  puts "clown costume than the last> Tehy haven't pulled their"
-  puts "weapons you tet, as they see the active bomb under your"
-  puts "arm and don't want to set it off."
+  puts <<-BRIDGE_HEREDOC
+    You burst onto the Bridge with the neutron destruct bomb
+    under your arm and surprise 5 Gothons who are trying to
+    take control of the ship. Each of them has an even uglier
+    clown costume than the last> They haven't pulled their
+    weapons out yet, as they see the active bomb under your
+    arm and don't want to set it off.
+  BRIDGE_HEREDOC
 
   prompt()
   action = gets.chomp()
 
   if action == "throw the bomb"
-    puts "In a panic you throw the bomb at the group of Gothons"
-    puts "and make a leap for the door. Right as you drop it a"
-    puts "Gothon shoots you right in the back killing you."
-    puts "As you die you see another Gothon frantically try to disarm"
-    puts "the bomb. You die knowing they will probably blow up when"
-    puts "it goes off."
+    puts <<-THROW_BOMB_HEREDOC
+      In a panic you throw the bomb at the group of Gothons
+      and make a leap for the door. Right as you drop it a
+      Gothon shoots you right in the back killing you.
+      As you die you see another Gothon frantically try to disarm
+      the bomb. You die knowing they will probably blow up when
+      it goes off.
+    THROW_BOMB_HEREDOC
     return :death
 
   elsif action == "slowly place the bomb"
-    puts "You point your blaster at teh bomb under your arm"
-    puts "and theGothons put their hands up and start to sweat."
-    puts "You inch bacward to the door, open it, and then carefully"
-    puts "place the bomb on the floor, pointing your blaster at it."
-    puts "You then jump back hrough the door, punch the close button"
-    puts "and blast the lock so the Gothons can't get out."
-    puts "Now that the bomb is placed you run to the escape pod to"
-    puts "et off this tin can."
+    puts <<-PLACE_BOMB_HEREDOC
+      You point your blaster at teh bomb under your arm
+      and theGothons put their hands up and start to sweat.
+      You inch bacward to the door, open it, and then carefully
+      place the bomb on the floor, pointing your blaster at it.
+      You then jump back hrough the door, punch the close button
+      and blast the lock so the Gothons can't get out.
+      Now that the bomb is placed you run to the escape pod to
+      get off this tin can.
+    PLACE_BOMB_HEREDOC
     return :escape_pod
 
   else
@@ -171,31 +186,37 @@ def the_bridge()
 end
 
 def escape_pod()
-  puts "You rush through the sip desperately trying to make it to"
-  puts "the escape pod before the whole ship explodes. It seems like"
-  puts "hardly any Gothons are on the sip, so your run is clear of"
-  puts "interference. You get to the chamber with the escape pods, and"
-  puts "now need to pick one to take. Some of them could be damaged"
-  puts "but you don't have time to look. There's 5 pods, which one"
-  puts "do you take?"
+  puts <<-ESCAPE_POD_HEREDOC
+    You rush through the sip desperately trying to make it to
+    the escape pod before the whole ship explodes. It seems like
+    hardly any Gothons are on the sip, so your run is clear of
+    interference. You get to the chamber with the escape pods, and
+    now need to pick one to take. Some of them could be damaged
+    but you don't have time to look. There's 5 pods, which one
+    do you take?
+  ESCAPE_POD_HEREDOC
 
   good_pod = rand(5)+1
   print "[pod #]>"
   guess = gets.chomp()
 
   if guess.to_i != good_pod
-    puts "you jump into pod %s and hit the eject button." % guess
-    puts "The pod escapes out into the void of space, then"
-    puts "implodes as the hull ruptures, crushing your doby"
-    puts "into jam jelly."
+    puts  "      You jump into pod %s and hit the eject button." % guess
+    puts <<-BAD_POD_HEREDOC
+      The pod escapes out into the void of space, then
+      implodes as the hull ruptures, crushing your doby
+      into jam jelly.
+    BAD_POD_HEREDOC
     return :death
   else
-    puts "The pod escapes out into the void of space, then"
-    puts "The pod easily slides out into space heading to"
-    puts "the planet below. As it flies to the planet, you look"
-    puts "back and see your ship implode then explode lik a"
-    puts "bright star, taking out the Gothon ship at the same"
-    puts "time. You won!"
+    puts <<-GOOD_POD_HEREDOC
+      The pod escapes out into the void of space, then
+      The pod easily slides out into space heading to
+      the planet below. As it flies to the planet, you look
+      back and see your ship implode then explode lik a
+      bright star, taking out the Gothon ship at the same
+      time. You won!
+    GOOD_POD_HEREDOC
     # exit with code 0 to indicate player won
     Process.exit(0)
   end
