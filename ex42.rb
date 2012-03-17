0025c7b29987247e02b213e8585fc8254d50e37f
beepscore <support@beepscore.com>
2012-03-17 10:52:59 -0700

    Change from one class Game to two classes Map and Engine.
    Add comments.

--- ex42.rb
+++ ex42.rb
@@ -2,45 +2,10 @@
 
 require 'pp'
 
-class Game
-
-  # initialize a game
-  # parameter start is a label representing the starting location
-  def initialize(start)
-    @quips = ["You died. You kinda suck at this.",
-              "So how's it feel to be dead?",
-              "Such a luser.",
-              "I have a small puppy that's better at this."]
-    @start = start
-    puts "in init @start = " + @start.inspect
-  end
+class Map
 
   def prompt()
     print "> "
-  end
-
-  def play()
-    puts "@start => " + @start.inspect
-    next_room = @start
-
-    while true
-      puts "\n---------"
-
-      # method(next_room) returns a Game method object
-      # method() instantiates a method object
-      # http://ruby-doc.org/core-1.9.3/Object.html#method-i-method
-      room = method(next_room)
-      # call the method named "room"
-      # room.call() returns a symbol of a method name e.g. :central_corridor
-      # Assign the return value to next_room
-      next_room = room.call()
-    end
-  end
-
-  def death()
-    puts @quips[rand(@quips.length())]
-    # exit with code 1 to indicate player died
-    Process.exit(1)
   end
 
   def central_corridor()
@@ -248,8 +213,60 @@ class Game
       Process.exit(0)
     end
   end
-end
+end #Map
+
+
+class Engine
+
+  # initialize a game
+  # parameter start is a label representing the starting location
+  def initialize(start)
+    @a_map = Map.new()
+    @quips = ["You died. You kinda suck at this.",
+              "So how's it feel to be dead?",
+              "Such a luser.",
+              "I have a small puppy that's better at this."]
+    @start = start
+    puts "in init @start = " + @start.inspect
+  end
+
+  def play()
+    puts "@start => " + @start.inspect
+    next_room_symbol = @start
+
+    while true
+      puts "\n---------"
+
+      if next_room_symbol == :death
+        # call Engine method death()
+        death()
+      else
+        # Symbol next_room_symbol has a correspondingly named method in object a_map.
+        # We will use the symbol to get a reference to a_map's method.
+        # method() instantiates a method object
+        # http://ruby-doc.org/core-1.9.3/Object.html#method-i-method
+        # Get a_map's method and assign it to room.
+        room = @a_map.method(next_room_symbol)
+
+        # Call "room", an a_map method
+        # room.call() returns a symbol e.g. :central_corridor, :death, :the_bridge
+        # Note the returned symbol may be defined in Engine but not in Map, e.g. :death
+        # Assign the return value to next_room_symbol
+        next_room_symbol = room.call()
+      end
+
+    end
+  end
+
+  def death()
+    puts @quips[rand(@quips.length())]
+    # exit with code 1 to indicate player died
+    Process.exit(1)
+  end
+
+end #Engine
 
 # new calls initialize method
-a_game = Game.new(:central_corridor)
+a_game = Engine.new(:central_corridor)
 a_game.play()
+
