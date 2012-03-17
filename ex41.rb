e85a1af57d05ecf99711773c61f981c8786e398d
beepscore <support@beepscore.com>
2012-03-16 20:44:56 -0700

    Extra credit. Use a heredoc.

--- ex41.rb
+++ ex41.rb
@@ -17,16 +17,20 @@ def death()
 end
 
 def central_corridor()
-  puts "The Gothons of Planet Percal #25 have invaded your ship and destroyed"
-  puts "your entire crew. You are the last surviving member and your last"
-  puts "mission is to get the neutron destruct bomb from the Weapons Armory,"
-  puts "put it in the bridge, and blow the ship up after getting into an "
-  puts "escape pod."
-  puts "\n"
-  puts "You're running down the central corridor to the Weapons Armory when"
-  puts "a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume"
-  puts "flowing around his hate filled body. He's blocking the door to the"
-  puts "Armory and about to pull a weapon to blast you."
+  # heredocs can start with << or with <<-
+  # if they start with <<, the ending delimeter must be in column 1.
+  puts <<-CENTRAL_CORRIDOR_HEREDOC
+    The Gothons of Planet Percal #25 have invaded your ship and destroyed
+    your entire crew. You are the last surviving member and your last
+    mission is to get the neutron destruct bomb from the Weapons Armory,
+    put it in the bridge, and blow the ship up after getting into an 
+    escape pod.
+
+    You're running down the central corridor to the Weapons Armory when
+    a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume
+    flowing around his hate filled body. He's blocking the door to the
+    Armory and about to pull a weapon to blast you.
+  CENTRAL_CORRIDOR_HEREDOC
 
   prompt()
   action = gets.chomp()
